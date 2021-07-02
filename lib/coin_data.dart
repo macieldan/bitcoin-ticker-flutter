c3b6141297ac646ceb030f3137c40159fcc322c3
Daniel Maciel <daniel_maciel.04@hotmail.com>
2021-07-01 22:59:55 -0300

    update

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String kApiKey = '7F2385DB-0A94-43EA-AD2A-12071F260C4D';
+const String kBaseurl = 'https://rest.coinapi.io/v1/exchangerate/';
+
+getCoinData() {}
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +35,25 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  CoinData(this.selectedCurrency);
+  final String selectedCurrency;
+  Future getCoinData() async {
+    Map<String, String> coinPrices = {};
+
+    for (String crypto in cryptoList) {
+      http.Response response =
+          await http.get('$kBaseurl$crypto/$selectedCurrency?apikey=$kApiKey');
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+
+        coinPrices[crypto] = data['rate'].toStringAsFixed(0);
+        print(coinPrices[crypto]);
+      } else {
+        print(response.statusCode);
+      }
+    }
+    return coinPrices;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +7,74 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  Map<String, String> coinRateMap = {};
+
+  void initState() {
+    super.initState();
+    coinRate(selectedCurrency);
+  }
+
+  List<DropdownMenuItem> getDropdownItens() {
+    List<DropdownMenuItem<String>> dropdownItens = [];
+    for (int i = 0; i < currenciesList.length; i++) {
+      String currency = currenciesList[i];
+      var newIten = DropdownMenuItem(child: Text(currency), value: currency);
+      dropdownItens.add(newIten);
+    }
+
+    return dropdownItens;
+  }
+
+  Column getBitCoinsItens() {
+    List<Padding> cardItens = [];
+    String courrentCoin;
+
+    for (String crypto in cryptoList) {
+      courrentCoin = coinRateMap[crypto];
+      //print(courrentCoin);
+      Padding newPadding = Padding(
+        padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+        child: Card(
+          color: Colors.lightBlueAccent,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+            child: Text(
+              '1 $crypto = $courrentCoin $selectedCurrency',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+      cardItens.add(newPadding);
+    }
+    return Column(
+      children: cardItens,
+    );
+  }
+
+  void coinRate(String selectedCurrency) async {
+    CoinData coinData = CoinData(selectedCurrency);
+
+    try {
+      var valor = await coinData.getCoinData();
+      setState(() {
+        coinRateMap = valor;
+        //rateCoinBTC = valor.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,33 +85,19 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          getBitCoinsItens(),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton(
+                value: selectedCurrency,
+                items: getDropdownItens(),
+                onChanged: (value) {
+                  selectedCurrency = value;
+                  coinRate(selectedCurrency);
+                }),
           ),
         ],
       ),
